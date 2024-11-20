df9d43ad68a93c7acdebae5ddea81effc50ecc59
Md. Iztihad Islam <iiztihad@gmail.com>
2024-11-20 23:01:51 +0600

    Added  navbar and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Banner from "./components/Banner/Banner";
+import CoinTable from "./components/CoinTable/CoinTable";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
 
   return (
     <>
-      
+      <Navbar />
+      <Banner />
+      <CoinTable />
     </>
   )
 }
