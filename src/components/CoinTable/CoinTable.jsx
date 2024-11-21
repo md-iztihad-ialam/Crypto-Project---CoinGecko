665abf95b41bd56e60c29b9531b98cf49d155c0f
Md. Iztihad Islam <iiztihad@gmail.com>
2024-11-21 22:40:38 +0600

    Added react query config

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import fetchCoinData from "../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
+
 function CoinTable(){
+
+    // useEffect(() => {
+    //     //console.log("CoinTable component mounted!");
+    //     fetchCoinData(1, 'usd');
+    // }, []) we have a better way instead to fatching with effect
+
+    const [page, setPage] = useState(1);
+    const {data, isLoading, isError, error, isFetching} = useQuery({
+        queryKey: ['coins', page],
+        queryFn: () => fetchCoinData(page, 'usd'),
+        retry: 2,
+        retryDelay: 1000,
+        cacheTime: 1000 * 60 * 2,
+    });
+
+    // useEffect(() => {
+    //     console.log(data);
+    // }, [data]);
+
+    if(isLoading){
+        return <div>Loading...</div>;
+    }
+    if(isError){
+        return <div>Error: {error.message}</div>;
+    }
 
     return(
         <>
             Coin Table
+            <button onClick={() => setPage(page+1)}>Click</button>
+            {page}
         </>
     );
 }
