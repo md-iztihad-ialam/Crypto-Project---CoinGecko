8936a113c4b7d52f2d745e9086154d717e5fb544
Md. Iztihad Islam <iiztihad@gmail.com>
2024-12-01 01:40:45 +0600

    added details page

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -5,31 +5,34 @@ import {lazy, Suspense} from "react";
 import MainLayout from "../../pages/Layout";
 import { Facebook } from "react-content-loader";
 import PageLoader from "../PageLoader/PageLoader";
+import CustomErrorBoundary from "../CustomErrorBoundary/CustomErrorBoundary";
 
 const Home = lazy(() => import("../../pages/Home"));
 const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));//lazily downloading this page
 
 function Routing(){
     return(
-        <Routes>
-            <Route path="/" element={<MainLayout />}>
-                <Route index element={
-                    
-                    <Suspense fallback={<PageLoader />}>
-                        <Home />
-                    </Suspense>
-                    
-                } />{/* this navbbar is the shared ui we want to have across pages*/}
-                <Route path="/details/:coinId" element={
-                    
-                    <Suspense fallback={<Facebook />}>
-                        <CoinDetailsPage />
-                    </Suspense>                    
-                    
-                }/> {/*the actual page which will be rendered along with navbar*/}
-                {/* this will render at the place of outlet */}
-            </Route> 
-        </Routes>
+        <CustomErrorBoundary>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={
+                        
+                        <Suspense fallback={<PageLoader />}>
+                            <Home />
+                        </Suspense>
+                        
+                    } />{/* this navbbar is the shared ui we want to have across pages*/}
+                    <Route path="/details/:coinId" element={
+                        
+                        <Suspense fallback={<Facebook />}>
+                            <CoinDetailsPage />
+                        </Suspense>                    
+                        
+                    }/> {/*the actual page which will be rendered along with navbar*/}
+                    {/* this will render at the place of outlet */}
+                </Route> 
+            </Routes>
+        </CustomErrorBoundary>
     );
 }
 
